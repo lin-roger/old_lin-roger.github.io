c79fa3eca5d39cf47b37583f4f3e132ecaa73f54
lin-roger <roger920707fb@gmail.com>
2021-11-02 02:24:06 +0800

    購買頁完成

--- js/a.js
+++ js/a.js
@@ -12,23 +12,33 @@ const i2p = document.querySelector("#i2 .p");
 const i2 = document.querySelector("#i2 p");
 const i2d = document.querySelector("#i2 .d");
 
-let i0num = 1,
-  i1num = 2,
-  i2num = 3;
+let i0num = 0,
+  i1num = 0,
+  i2num = 0;
+
+q0();
 
 enter.addEventListener("click", src);
 close.addEventListener("click", closef);
 
-i0p.addEventListener("click", ad(1, 0));
-i0d.addEventListener("click", ad(0, 0));
-i1p.addEventListener("click", ad(1, 1));
-i1d.addEventListener("click", ad(0, 1));
-i2p.addEventListener("click", ad(1, 2));
-i2d.addEventListener("click", ad(0, 2));
-
-function newFunction() {
-  i0p.addEventListener("click");
-}
+i0p.addEventListener("click", function () {
+  ad(1, 0);
+});
+i0d.addEventListener("click", function () {
+  ad(0, 0);
+});
+i1p.addEventListener("click", function () {
+  ad(1, 1);
+});
+i1d.addEventListener("click", function () {
+  ad(0, 1);
+});
+i2p.addEventListener("click", function () {
+  ad(1, 2);
+});
+i2d.addEventListener("click", function () {
+  ad(0, 2);
+});
 
 function src() {
   shop.style.visibility = "visible";
@@ -38,8 +48,27 @@ function closef() {
   shop.style.visibility = "hidden";
   document.body.style.overflow = "scroll";
 }
+function q0() {
+  if (i0num == 0) {
+    i0d.disabled = true;
+  }
+  else{
+    i0d.disabled = false;
+  }
+  if (i1num == 0) {
+    i1d.disabled = true;
+  }
+  else{
+    i1d.disabled = false;
+  }
+  if (i2num == 0) {
+    i2d.disabled = true;
+  }
+  else{
+    i2d.disabled = false;
+  }
+}
 function ad(c, d) {
-  console.log("call");
   switch (d) {
     case 0:
       switch (c) {
@@ -75,8 +104,9 @@ function ad(c, d) {
       }
       break;
   }
-  total.innerHTML = `共 ${i0num * 300 + i1num * 200 + i2num * 300} 元`;
+  total.innerHTML = `共 ${i0num * 300 + i1num * 200 + i2num * 100} 元`;
   i0.innerHTML = i0num;
   i1.innerHTML = i1num;
   i2.innerHTML = i2num;
+  q0();
 }
